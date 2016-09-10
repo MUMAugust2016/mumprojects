423c76b8f349cd316cf0d8e798eb17429b0c2225
sam <sam@10.10.11.96>
2016-09-09 20:46:17 -0500

    Source folder added, Lesson1 package added

---
+++ src/lesson1/GroupMembers.java
@@ -0,0 +1,21 @@
+package lesson1;
+public class GroupMembers {
+
+	/*
+	 * @Author Samuel
+	 * A Program to list all my team mebers and thier
+	 * ID, Group Number is 4
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String m1 = "Samuel Fentahuen";
+		int m1Id= 985438;
+		
+		System.out.println("==========================My Group 4 Members==========");
+		System.out.println("Member Name: " + m1 + ", and ID is: " + m1Id);
+		
+		
+	}
+
+}
